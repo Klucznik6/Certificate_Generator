b3737db2cb42019fc9ee9ae68a843182cd793878
Klucznik <kulikiewiczwiktor@gmail.com>
2024-05-19 15:47:21 +0200

    sender_and_receiver_email

--- pathsAndMail.py
+++ pathsAndMail.py
@@ -1,4 +1,13 @@
+import os
+##### scieżka szablonu = templatePath
+##### ścieżka pliku xlsx = xlsxPath
+##### ścieżka szablonów = templatesFolderPath
 programPath = os.getcwd()
 templatesFolderPath = programPath + '\\templates'
 pdfLookup = programPath + '\\lookup\\szablon.png'
 popplerPath = programPath + '\\poppler-23.07.0\\Library\\bin'
+
+#####
+senderEmail = ''
+senderPassword = ''
+receiverEmail = ''
